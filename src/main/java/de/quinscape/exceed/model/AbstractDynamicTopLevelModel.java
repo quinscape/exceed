4a7e7df63fede8b32571618184a04a64e894797e
Sven Helmberger <fforw@gmx.de>
2015-10-28 21:16:41 +0100

    Model: Introduce AbstractDynamicTopLevelModel
    Refactor the mostly dynamic layout model into an AbstractDynamic-
    TopLevelModel that functions as adapter from exceed models to svenson
    dynamic properties.

---
+++ src/main/java/de/quinscape/exceed/model/AbstractDynamicTopLevelModel.java
@@ -0,0 +1,74 @@
+package de.quinscape.exceed.model;
+
+import org.svenson.DynamicProperties;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Created by sven on 28.10.15.
+ */
+public class AbstractDynamicTopLevelModel
+    extends TopLevelModel
+    implements DynamicProperties
+{
+    private Map<String, Object> storage;
+
+
+    @Override
+    public void setProperty(String s, Object o)
+    {
+        if (storage == null)
+        {
+            storage = new HashMap<>();
+        }
+
+        storage.put(s, o);
+
+    }
+
+
+    @Override
+    public Object getProperty(String s)
+    {
+        if (storage == null)
+        {
+            return null;
+        }
+        return storage.get(s);
+    }
+
+
+    @Override
+    public Set<String> propertyNames()
+    {
+        if (storage == null)
+        {
+            return Collections.emptySet();
+        }
+
+        return storage.keySet();
+    }
+
+
+    @Override
+    public boolean hasProperty(String s)
+    {
+        if (storage == null)
+            return false;
+        return storage.keySet().contains(s);
+    }
+
+
+    @Override
+    public Object removeProperty(String s)
+    {
+        if (storage == null)
+        {
+            return null;
+        }
+        return storage.remove(s);
+    }
+}
--- src/main/java/de/quinscape/exceed/model/Layout.java
+++ src/main/java/de/quinscape/exceed/model/Layout.java
@@ -8,64 +8,8 @@ import java.util.Map;
 import java.util.Set;
 
 public class Layout
-    extends TopLevelModel
-    implements DynamicProperties
+    extends AbstractDynamicTopLevelModel
 {
-    private Map<String, Object> storage;
 
 
-    @Override
-    public void setProperty(String s, Object o)
-    {
-        if (storage == null)
-        {
-            storage = new HashMap<>();
-        }
-
-        storage.put(s, o);
-
-    }
-
-
-    @Override
-    public Object getProperty(String s)
-    {
-        if (storage == null)
-        {
-            return null;
-        }
-        return storage.get(s);
-    }
-
-
-    @Override
-    public Set<String> propertyNames()
-    {
-        if (storage == null)
-        {
-            return Collections.emptySet();
-        }
-
-        return storage.keySet();
-    }
-
-
-    @Override
-    public boolean hasProperty(String s)
-    {
-        if (storage == null)
-            return false;
-        return storage.keySet().contains(s);
-    }
-
-
-    @Override
-    public Object removeProperty(String s)
-    {
-        if (storage == null)
-        {
-            return null;
-        }
-        return storage.remove(s);
-    }
 }
