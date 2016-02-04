f8e3e5daaeb76fb399119639e815f1b0ac39dc50
Sven Helmberger <fforw@gmx.de>
2016-02-04 16:55:00 +0100

    fetch-view module
    Provide a dedicated service to refetch the current view, optionally with
    a new view model (editor preview).

---
+++ src/main/js/service/fetch-view.js
@@ -0,0 +1,35 @@
+var ajax = require("./ajax");
+
+/**
+ * Refetches the current view from the server via ajax.
+ *
+ * @param model     Model version to preview, if not given, fetchView will just re-GET the view via AJAX
+ * @returns {Promise}
+ */
+module.exports = function(model)
+{
+
+    var isPreview = !!model;
+    //console.log("fetchView, isPreview = ", isPreview, JSON.stringify(model, null, "  "));
+
+    var opts = {
+        contentType: "JSON",
+        url: window.location.href
+    };
+
+    if (isPreview)
+    {
+        opts.method =  "POST";
+        opts.data = JSON.stringify(model);
+        opts.headers = {
+            "X-ceed-Preview" : "true"
+        }
+    }
+    else
+    {
+        opts.method =  "GET";
+    }
+
+    return ajax(opts);
+};
+
