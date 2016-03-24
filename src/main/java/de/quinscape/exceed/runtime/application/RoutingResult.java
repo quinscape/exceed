ccaf15a89bd522ac6357dd8c5484ebd436ad59f5
Sven Helmberger <sven.helmberger@quinscape.de>
2016-03-24 22:19:56 +0100

    routing fixup

--- src/main/java/de/quinscape/exceed/runtime/application/RoutingResult.java
+++ src/main/java/de/quinscape/exceed/runtime/application/RoutingResult.java
@@ -2,6 +2,8 @@ package de.quinscape.exceed.runtime.application;
 
 import de.quinscape.exceed.model.routing.Mapping;
 
+import java.util.Map;
+
 /**
  * Encapsulates the result of the routing table resolution process.
  */
@@ -9,13 +11,23 @@ public class RoutingResult
 {
     private final Mapping mapping;
 
-    public RoutingResult(Mapping mapping)
+    private final Map<String, String> variables;
+
+
+    public RoutingResult(Mapping mapping, Map<String, String> variables)
     {
         this.mapping = mapping;
+        this.variables = variables;
     }
 
     public Mapping getMapping()
     {
         return mapping;
+    }
+
+
+    public Map<String, String> getVariables()
+    {
+        return variables;
     }
 }
