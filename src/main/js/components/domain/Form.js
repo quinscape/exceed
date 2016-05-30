acb270bba28a975bb7b2741b36558205f3bd614c
Sven Helmberger <sven.helmberger@quinscape.de>
2016-05-31 00:08:33 +0200

    Form: Accept domain objects directly
    This commit makes Form accept two kinds of data-input:
    
     * DataList JSON
     * domain Object JSON
    
    DataList JSON is most often the result of a resolved query expressions
    which is very handy but also a bit cumbersome for the process content.
    
    We make the form accept domain object JSON directly by detecting it and
    wrapping it with the equivalent data list that has that domain object as
    only result. We receive the necessary type information via the new
    scope.js service which offers some methods for dealing with scoped
    objects.
    
    Scope.list(name) , Scope.object(name), Scope.propery(name) access scoped
    objects of that type. The scope service calls are automatically detected
    via our babel-plugin-track-usage and thus have to be string literals only
    at this point. ( no property("myValue" + 5) or so).
    
    Scope.objectType(name) provides the domain type structure for the scoped
    object of that name including the special "current" object.
    
    We change the data prop to the both case covering name "data".
    
    The form data can be applied via expression by using "data" as
    attribute, thus overriding the query mechanism.
    
    Editor-XML Example:
    
    	<Form id="myForm" data="{ object('current') }">
    	...
    	</Form>

--- src/main/js/components/domain/Form.js
+++ src/main/js/components/domain/Form.js
@@ -2,11 +2,55 @@ var React = require("react");
 var cx = require("classnames");
 
 var DataList = require("../../util/data-list");
+var Scope = require("../../service/scope");
 
 var FormContext = require("./form-context");
 
 var LinkedStateMixin = require("react-addons-linked-state-mixin");
 
+function isRawDataList(input)
+{
+    return input && input.types && input.rows && input.columns;
+}
+
+
+
+function objectToDataList(input)
+{
+    var typeName = input._type;
+    var domainType = Scope.objectType(typeName);
+
+    var cols = {};
+    var obj = {};
+    for (var name in input)
+    {
+        if (input.hasOwnProperty(name))
+        {
+            if (name === "_type")
+            {
+                obj[name] = input[name];
+            }
+            else
+            {
+                var qualified = typeName + "." + name;
+                obj[qualified] = input[name];
+                cols[qualified] = {
+                    type: typeName,
+                    name: name
+                };
+            }
+        }
+    }
+
+    return {
+        types: {
+            [typeName] : domainType
+        },
+        columns: cols,
+        rows: [ obj ],
+        rowCount: 1
+    };
+}
 var Form = React.createClass({
 
     mixins: [ LinkedStateMixin ],
@@ -29,10 +73,11 @@ var Form = React.createClass({
     },
 
     propTypes: {
-        dataList: React.PropTypes.object.isRequired,
+        data: React.PropTypes.object.isRequired,
         horizontal: React.PropTypes.bool,
         labelClass: React.PropTypes.string,
-        wrapperClass: React.PropTypes.string
+        wrapperClass: React.PropTypes.string,
+        index: React.PropTypes.number
     },
 
     getDefaultProps: function ()
@@ -40,14 +85,36 @@ var Form = React.createClass({
         return {
             horizontal: true,
             labelClass: "col-md-2",
-            wrapperClass: "col-md-4"
+            wrapperClass: "col-md-4",
+            index: 0
         };
     },
 
+    getDataList: function(input)
+    {
+        if (!input)
+        {
+            throw new Error("No data");
+        }
+
+        if (isRawDataList(input))
+        {
+            return new DataList(input, this.onChange);
+        }
+        else if (input._type)
+        {
+            return new DataList( objectToDataList(input), this.onChange);
+
+        }
+        else
+        {
+            console.error("Cannot handle data", input);
+        }
+    },
     getInitialState: function ()
     {
         return {
-            dataList: new DataList(this.props.dataList, this.onChange),
+            dataList: this.getDataList(this.props.data),
             errors: {}
         };
     },
@@ -57,7 +124,7 @@ var Form = React.createClass({
     {
         console.log("onChange", JSON.stringify(newRows), path);
 
-        var dataList = this.state.dataList;
+        var dataList =  new DataList(this.state.dataList);
         dataList.rows = newRows;
 
         this.setState({
@@ -69,7 +136,7 @@ var Form = React.createClass({
     {
         //console.log("NEXTPROPS", nextProps);
 
-        if (nextProps.dataList.rows[0] !== this.props.dataList.rows[0])
+        if (nextProps.dataList !== this.props.data)
         {
             // XXX: merge changed props in current state with new state?
 
@@ -83,14 +150,14 @@ var Form = React.createClass({
     {
         var dataList = this.state.dataList;
 
-        //console.log(this.props.dataList);
+        //console.log(this.props.data);
 
         if (dataList.rowCount != 1)
         {
             throw new Error("Need exactly 1 result here");
         }
 
-        var cursor = dataList.getCursor([0]);
+        var cursor = dataList.getCursor([this.props.index]);
 
         //console.log("CURSOR", cursor);
 
