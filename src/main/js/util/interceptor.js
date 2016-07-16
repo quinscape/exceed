629e97014615660702ae52d068d9259b5899f757
Sven Helmberger <sven.helmberger@quinscape.de>
2016-07-16 18:31:23 +0200

    Interceptor testing helper
    When testing modules, proxyquire can be really helpful to mock dependencies
    for the purpose of the test, so we are using that in any case.
    
    Another pattern that has proven helpful and which is called interceptor
    here is a silly little module that can help a lot.
    
        module.exports = function (data)
        {
            return data || {}
        };
    
    Just that as individual module allows other modules requiring it to
    either create internal buffers or expose internal buffers to this module
    we can then proxyquire away in our tests.
    
    All while shielding the export from accidental use / change during
    normal operation.

---
+++ src/main/js/util/interceptor.js
@@ -0,0 +1,17 @@
+/**
+ * Helper module for proxyquire based js testing. Clients go
+ *
+ * const INTERNAL_BUFFER = require("./interceptor")();
+ *
+ * and tests can proxiquire exactly that require to get access to internal buffers for testing while making it impossible
+ * to accidentally mess with those structures without proxyquire.
+ *
+ * @param [data]    {*} if given truthy, this is the value that will be returned again, otherwise it's a new array. The value
+ *                      can also be used to qualify multiple interceptor calls in a single module
+ * @returns passed through data or a new object.
+ */
+module.exports = function (data)
+{
+    return data || {}
+};
+
