7e5bfc419bedd654440cc40f8bb8e43569e3f234
Sven Helmberger <sven.helmberger@quinscape.de>
2017-02-24 21:49:55 +0100

    Cando: Add rerunFeatureTests
    Refactor cando module and offer export rerunFeatureTests that can be
    used to rerun cando feature tests in a mocked environment to make sure
    they're up-to-date.

--- src/main/js/cando.js
+++ src/main/js/cando.js
@@ -1,68 +1,89 @@
-var createXMLHTTPObject = require("./util/xhr-factory");
-var xmlParser = require("./util/parseXML");
+const assign = require("object-assign");
+const createXMLHTTPObject = require("./util/xhr-factory");
+const xmlParser = require("./util/parseXML");
 
 /**
  * Runs feature detections and stores the result for future use.
  *
  * @type {{pushState: boolean, ajax: boolean, localStorage: boolean, sessionStorage: boolean, svg: boolean, parseXML: boolean }}
  */
-module.exports = {
-    /**
-     * Browser supports history.pushState()
-     */
-    pushState: typeof window !== "undefined" && "pushState" in window.history && typeof window.history["pushState"] === "function",
-    /**
-     * Browser supports AJAX
-     */
-    ajax: !!createXMLHTTPObject(),
 
-    localStorage: (function ()
-    {
-        try
-        {
-            return typeof window !== "undefined" && "localStorage" in window && window["localStorage"];
-        }
-        catch (e)
-        {
-            return false;
-        }
-    })(),
+var Features = {};
 
-    sessionStorage: (function ()
-    {
-        try
-        {
-            return typeof window !== "undefined" &&"sessionStorage" in window && window["sessionStorage"];
-        }
-        catch (e)
-        {
-            return false;
-        }
-    })(),
+function run()
+{
 
-    svg: (function ()
-    {
-        try
-        {
-            return typeof document !== "undefined" && !!document.createElementNS && !!document.createElementNS('http://www.w3.org/2000/svg', "svg").createSVGRect;
-        }
-        catch (e)
-        {
-            return false;
-        }
-    })(),
+    assign(Features, {
+        /**
+         * Browser supports history.pushState()
+         */
+        pushState: typeof window !== "undefined" && window.history && "pushState" in window.history && typeof window.history["pushState"] === "function",
+        /**
+         * Browser supports AJAX
+         */
+        ajax: !!createXMLHTTPObject(),
 
-    webSockets: (function ()
-    {
-        try
+        localStorage: (function ()
         {
-            return typeof window !== "undefined" && "WebSocket" in window && window["WebSocket"];
-        }
-        catch(e)
-        {
-            return false;
-        }
-    })(),
+            try
+            {
+                return typeof window !== "undefined" && "localStorage" in window && !!window["localStorage"];
+            }
+            catch (e)
+            {
+                return false;
+            }
+        })(),
 
-    parseXML: !!xmlParser
-};
+        sessionStorage: (function ()
+        {
+            try
+            {
+                return typeof window !== "undefined" && "sessionStorage" in window && !!window["sessionStorage"];
+            }
+            catch (e)
+            {
+                return false;
+            }
+        })(),
+
+        svg: (function ()
+        {
+            try
+            {
+                return typeof document !== "undefined" && !!document.createElementNS && !!document.createElementNS('http://www.w3.org/2000/svg', "svg").createSVGRect;
+            }
+            catch (e)
+            {
+                return false;
+            }
+        })(),
+
+        webSockets: (function ()
+        {
+            try
+            {
+                return typeof window !== "undefined" && "WebSocket" in window && !!window["WebSocket"];
+            }
+            catch (e)
+            {
+                return false;
+            }
+        })(),
+
+        parseXML: !!xmlParser
+    });
+}
+
+run();
+/**
+ * Should only be used to redo testing with a new mocked environment.
+ *
+ * @type {function}
+ */
+Features.rerunFeatureTests = run;
+
+module.exports = Features;
+
+
+
