d73568794874c321e302645855193b8cf0ea43ac
Sven Helmberger <sven.helmberger@quinscape.de>
2017-10-26 17:39:03 +0200

    Build: Make GenerateResourceLists configurable
    Configure our ad-hoc GenerateResourceList tool to work off two lists of
    semicolon-separated lists.
    
    The first one lists all source directories to create a resource list
    for.
    
    The second one lists the target directories corresponding to the first
    list. Both lists must match in number of items, obviously.
    
    Wildcard operation
    ------------------
    
    We also allow a source directory to end in "/*" in which case we will
    generate a resource list for every direct sub-directory of the source
    directory.
    
    The target needs to be the corresponding parent folder
    
    So for example with this definition:
    
        <plugin>
            <groupId>org.codehaus.mojo</groupId>
            <artifactId>exec-maven-plugin</artifactId>
            <version>1.4.0</version>
            <executions>
                <execution>
                    <id>generate-resource-lists</id>
                    <phase>generate-resources</phase>
                    <goals>
                        <goal>
                            java
                        </goal>
                    </goals>
                    <configuration>
                        <mainClass>de.quinscape.exceed.tooling.GenerateLibraryResourceLists</mainClass>
                        <arguments>
                            <argument>src/main/webapp/WEB-INF/extensions/*</argument>
                            <argument>target/exceed-app/WEB-INF/extensions</argument>
                        </arguments>
                    </configuration>
                </execution>
            </executions>
        </plugin>
    
    We would generate resource lists for src/main/webapp/WEB-INF/extensions/extension-a and
    src/main/webapp/WEB-INF/extensions/extension-b and the resource list files would end up under
    target/exceed-app/WEB-INF/extensions/extension-a/resource.lst and
    target/exceed-app/WEB-INF/extensions/extension-b/resource.lst

--- src/main/java/de/quinscape/exceed/tooling/GenerateLibraryResourceLists.java
+++ src/main/java/de/quinscape/exceed/tooling/GenerateLibraryResourceLists.java
@@ -3,11 +3,13 @@ package de.quinscape.exceed.tooling;
 import de.quinscape.exceed.runtime.util.FileExtension;
 import de.quinscape.exceed.runtime.util.Util;
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.filefilter.FalseFileFilter;
 import org.apache.commons.io.filefilter.IOFileFilter;
 import org.apache.commons.io.filefilter.TrueFileFilter;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
 /**
  * Generates a list of module resources packaged with the exceed library
@@ -23,15 +25,60 @@ public class GenerateLibraryResourceLists
 
         GenerateLibraryResourceLists tool = new GenerateLibraryResourceLists();
 
-        tool.generateResourceList(
-            "src/main/base",
-            "target/classes/de/quinscape/exceed/base/resource.lst",
-            TrueFileFilter.INSTANCE);
+        if (args.length != 2)
+        {
+            throw new IllegalArgumentException("GenerateLibraryResourceLists takes exatcly 2 arguments (sourceDirList, targetFileList) both semicolon separated");
+        }
 
-        tool.generateResourceList(
-            "src/main/js/components",
-            "target/classes/de/quinscape/exceed/components/resource.lst",
-            NonJsComponentFileFilter.INSTANCE);
+        List<String> sourceDirList = Util.split(args[0], ";");
+        List<String> targetFileList = Util.split(args[1], ";");
+
+        if (sourceDirList.size() != targetFileList.size())
+        {
+            throw new IllegalArgumentException("sourceDirList has "+ sourceDirList.size() + " entries, but targetFileList has " + targetFileList.size() + ": must be the same" );
+        }
+
+        for (int i = 0; i < sourceDirList.size(); i++)
+        {
+            String sourceDir = sourceDirList.get(i);
+            String targetFile = targetFileList.get(i);
+
+            if (sourceDir.endsWith("/*"))
+            {
+                final int baseLength = sourceDir.length() - 2;
+                final File wildcardBase = new File(sourceDir.substring(0, baseLength));
+
+                System.out.println("BASE " + wildcardBase);
+
+                if (!wildcardBase.exists())
+                {
+                    throw new IllegalArgumentException(wildcardBase + " does not exist");
+                }
+
+                for (File file : FileUtils.listFilesAndDirs(wildcardBase, FalseFileFilter.INSTANCE, new SubDirFilter(wildcardBase)))
+                {
+                    final String path = file.getPath();
+
+                    String target = targetFile + path.substring(baseLength) + "/resource.lst";
+
+                    System.out.println("GENERATE " + path + " => " + target);
+
+                    tool.generateResourceList(
+                        path,
+                        target,
+                        NonJsComponentFileFilter.INSTANCE
+                    );
+                }
+            }
+            else
+            {
+                tool.generateResourceList(
+                    sourceDir,
+                    targetFile,
+                    NonJsComponentFileFilter.INSTANCE);
+
+            }
+        }
     }
 
 
@@ -72,6 +119,32 @@ public class GenerateLibraryResourceLists
         public boolean accept(File dir, String name)
         {
             return match(name);
+        }
+    }
+
+    private static class SubDirFilter
+        implements IOFileFilter
+    {
+        private final File wildcardBase;
+
+
+        public SubDirFilter(File wildcardBase)
+        {
+            this.wildcardBase = wildcardBase;
+        }
+
+
+        @Override
+        public boolean accept(File file)
+        {
+            return file.isDirectory() && file.getParentFile().equals(wildcardBase);
+        }
+
+
+        @Override
+        public boolean accept(File file, String s)
+        {
+            return accept(new File(file, s));
         }
     }
 }
